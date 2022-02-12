1afb0b29b2c47069b1c578d2cbcdd4444fce4f27
Brian Ridge <brian.ridge@evolve.com>
2022-02-12 10:58:10 -0700

    setup with architect starter, add font

---
+++ app/routes/index.tsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div className="myFontClass">
+      <h1>Welcome to Remix</h1>
+      <p>Does my locally hosted Nunito font work?</p>
+    </div>
+  );
+}
